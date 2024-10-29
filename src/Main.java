d0aa1f646ab5fb35eb0adfac55b162c03a21f535
Mario Olcina <marolccon@alu.edu.gva.es>
2024-10-29 12:00:15 +0100

    Agregado el código

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 20; i += 2) {
+            if (i == 16)
+                continue;
+            System.out.println(i);
+        }
+
+        for (int i = 10; i <= 20; i++) {
+            if (i % 2 == 1)
+                continue;
+            if (i == 16)
+                continue;
+
+            System.out.println(i);
+        }
+
+        for (int i = 10;; i += 2) {
+            if (i == 16)
+                continue;
+            if (i > 20)
+                break;
+
+            System.out.println(i);
+
+        }
+    }
+}
+
